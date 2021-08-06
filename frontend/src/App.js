0d5892cce3bbba3047935a1f8549265acc8e47e9
TomZe1982 <thomas.zeisner@gmail.com>
2021-08-06 11:42:43 +0200

    fixed node_module deleted from toplevel

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/Header.js'
+import {Header} from "./components/Header";
+
+function App() {
+  return (
+    <div className="TodoApp">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,8 @@
+import './Header.css'
+
+export function Header() {
+   return <header>
+       <img className="HeaderImage" src="https://ih1.redbubble.net/image.342694287.3487/st,small,507x507-pad,600x600,f8f8f8.jpg" alt=""/>
+        <h1>Get Your *Shit* done</h1>
+    </header>
+}
